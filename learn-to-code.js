57eb79441add38b8d2b1ddaa9c98016a32b35063
Daniel Colangelo <Danalfons@yahoo.com>
2017-04-14 01:27:19 -0400

    learning javascript

---
+++ learn-to-code.js
@@ -0,0 +1,13 @@
+/*var name = "jack";
+var age = 23;
+
+var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+console.log(message);*/
+
+/*var firstName = "john";
+var lastName = "jake";
+var birthDate = "jan 1984";
+var age = 43;
+
+var loginMessage = "welcome, " + firstName;*/
