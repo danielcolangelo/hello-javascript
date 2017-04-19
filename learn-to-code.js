b4703e8cbff18dfa596ac7c4e906f263bde849e0
Daniel Colangelo <Danalfons@yahoo.com>
2017-04-19 03:39:00 -0400

    learning objects

--- learn-to-code.js
+++ learn-to-code.js
@@ -29,3 +29,74 @@ var loginMessage = "welcome, " + firstName;*/
 //for (var i = 0; i < 15; i++) {
 //	console.log(i);
 //}
+
+// var length1 = 15;
+// var width1 = 10;
+// var area1 = length1 * width1;
+//
+// var length2 = 12;
+// var width2 = 14;
+// var area2 = length2 * width2;
+
+// function area(length, width) {
+//   return length * width;
+// }
+//
+// var area1 = area(10, 15);
+// console.log(area1);
+//
+// var bankBalance = 500;
+// function makeTransaction(priceOfSale) {
+//   if (priceOfSale <= bankBalance) {
+//     bankBalance -= priceOfSale;
+//     console.log("Purchased!");
+//   } else {
+//     console.log("Insufficient funds!");
+//   }
+// }
+//
+// console.log(bankBalance);
+// makeTransaction(79.00);
+//
+// console.log(bankBalance);
+// makeTransaction(2.32);
+//
+// console.log(bankBalance);
+// makeTransaction(10.45);
+//
+// console.log(bankBalance);
+// makeTransaction(450.00);
+//
+// var transaction = function(priceOfSale) {
+//   if (priceOfSale <= bankBalance) {
+//     bankBalance -= priceOfSale;
+//     console.log("Purchased!");
+//   } else {
+//     console.log("Insufficient funds!");
+//   }
+// };
+//
+// var printCustomerName = function(first, last) {
+//   console.log("First Name: " + name + "Last Name: " + last);
+// }
+//
+// var applyForCreditCard = function(creditScore, soul) {
+//   //call some crazy function to process app
+// }
+//
+// var bankOperations = [];
+// var bankOperations.push(printCustomerName);
+// var bankOperations.push(applyForCreditCard);
+// var bankOperations.push(applyForCreditCard);
+
+
+var student = {
+  firstName: "john",
+  lastName: "Parker",
+  age: 7
+};
+
+
+
+console.log(student.firstName);
+console.log(student.lastName);
